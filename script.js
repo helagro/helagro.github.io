4513afe25d1abdd80de61f3e864eb4e4d0c3f607
Henrik Lagrosen <helagro@outlook.com>
2025-06-12 18:54:03 +0200

    Update navigation

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-
-function onHover(){
+function onHover() {
     document.activeElement.blur()
 }
+
+let timeout = null
+
+document.addEventListener('keydown', event => {
+    if (event.altKey) {
+        if (timeout) clearTimeout(timeout)
+
+        document.body.classList.add('showHidden')
+    }
+})
+
+document.addEventListener('keyup', event => {
+    if (!event.altKey) {
+        timeout = setTimeout(() => {
+            document.body.classList.remove('showHidden')
+        }, 4000)
+    }
+})
